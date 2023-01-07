7bd6fb9fd9adf8985793cff4cf305696f78b9bdc
HaeRheeNam <hanm3208@gmail.com>
2023-01-07 22:16:39 +0900

    현재 위치 state값 설명 수정

--- src/Components/Kakaospot/MapContainer.jsx
+++ src/Components/Kakaospot/MapContainer.jsx
@@ -144,6 +144,7 @@ const MapContainer = ({ searchPlace }) => {
   //currSpot.currW, currSpot.currK
   console.log("인풋값 좌표", searchWK);
   //searchWk.searchW , searchWK.searchK
+
   //현재 위치값~ 인풋 출발역 값 좌표 두 지점 간의 거리
   //계산 라이브러리 geolib => getDistance() 함수
   //getDistance(위도, 경도, )
@@ -158,6 +159,7 @@ const MapContainer = ({ searchPlace }) => {
     }
   );
   console.log(WKdata);
+
   return (
     <div>
       <div
